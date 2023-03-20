26e821c04baf4ec8b0f0f2067d179b669b5c37ca
luisfegz <luisfelipegaitanzambrano@gmail.com>
2023-03-20 16:24:21 -0500

    Update Files from local to gitHub
    We add src, test, and CMakeList

---
+++ src/Hospedaje.cpp
@@ -0,0 +1,5 @@
+//
+// Created by acer on 20/03/2023.
+//
+
+#include "Hospedaje.h"
---
+++ src/Hospedaje.h
@@ -0,0 +1,14 @@
+//
+// Created by acer on 20/03/2023.
+//
+
+#ifndef PROYECT1_HOSPEDAJE_H
+#define PROYECT1_HOSPEDAJE_H
+
+
+class Hospedaje {
+
+};
+
+
+#endif //PROYECT1_HOSPEDAJE_H
